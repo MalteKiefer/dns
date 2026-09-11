71f5a0efe2ff163297ef0e31bff627457422aac9
Malte Kiefer <malte.kiefer@mailgermania.de>
2026-09-11 03:17:50 +0200

    added pm

--- dnsconfig.js
+++ dnsconfig.js
@@ -56,6 +56,8 @@ D(DOMAIN, REG_INWX, DnsProvider(DSP_DESEC),
     AAAA("stats", IP6_WEB),
 	A("sync", IP4_WEB),
     AAAA("sync", IP6_WEB),
+	A("pm", IP4_WEB),
+    AAAA("pm", IP6_WEB),
 
     // Mail server host: mail is served by mailbox.org now, the box itself
     // stays reachable under this name.
